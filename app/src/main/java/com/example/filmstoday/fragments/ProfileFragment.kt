0a238f6d556d3ea36e9024a9ac92c5052a1b22e5
Alex <bamako99@mail.ru>
2021-02-11 17:36:42 +0300

    Сделана заготовка под экран профиля

--- app/src/main/java/com/example/filmstoday/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/filmstoday/fragments/ProfileFragment.kt
@@ -1,17 +1,32 @@
 package com.example.filmstoday.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.filmstoday.R
+import com.example.filmstoday.databinding.FragmentProfileBinding
+import com.example.filmstoday.viewmodels.ProfileViewModel
 
 class ProfileFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    private lateinit var binding: FragmentProfileBinding
+    private lateinit var profileViewModel: ProfileViewModel
 
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.
+            inflate(inflater, R.layout.fragment_profile, container, false)
+
+        profileViewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
     }
 }
