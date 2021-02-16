01e8fdf61faea47bd6bfdc91d601550d2c3d7f72
Алексей Рогатин <bamako99@mail.ru>
2021-02-16 10:57:43 +0300

    Делаю домашку

--- app/src/main/java/com/example/filmstoday/activities/MainActivity.kt
+++ app/src/main/java/com/example/filmstoday/activities/MainActivity.kt
@@ -1,25 +1,45 @@
 package com.example.filmstoday.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import com.example.filmstoday.BuildConfig
 import com.example.filmstoday.R
 import com.example.filmstoday.databinding.MainActivityBinding
+import com.example.filmstoday.viewmodels.MainActivityViewModel
+
 
 class MainActivity : AppCompatActivity() {
 
+    private val TAG = "MainActivity"
+
+    private val mainActivityViewModel: MainActivityViewModel by lazy {
+        ViewModelProvider(this).get(MainActivityViewModel::class.java)
+    }
+
     private lateinit var binding: MainActivityBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupNavigation()
+        setupConnectionListener()
+    }
 
+    private fun setupNavigation() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bottomNavigationView.setupWithNavController(navController)
+    }
+
+    private fun setupConnectionListener() {
+        val iFNetwork = IntentFilter()
+        iFNetwork.addAction(ConnectivityManager.CONNECTIVITY_ACTION)
     }
 }
---
+++ app/src/main/java/com/example/filmstoday/viewmodels/MainActivityViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.filmstoday.viewmodels
+
+import androidx.lifecycle.*
+
+class MainActivityViewModel: ViewModel(), LifecycleObserver {
+
+}
