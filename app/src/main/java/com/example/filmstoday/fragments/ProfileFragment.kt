d584202f99d225e8800b46f6e8d62fd2c0e1cc52
Алексей Рогатин <bamako99@mail.ru>
2021-02-26 17:03:12 +0300

    Сделан экран с настройками

--- app/src/main/java/com/example/filmstoday/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/filmstoday/fragments/ProfileFragment.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.filmstoday.R
 import com.example.filmstoday.databinding.FragmentProfileBinding
 import com.example.filmstoday.viewmodels.ProfileViewModel
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class ProfileFragment : Fragment() {
 
@@ -18,6 +21,8 @@ class ProfileFragment : Fragment() {
     }
 
     private lateinit var binding: FragmentProfileBinding
+    private lateinit var profileBottomSheet: View
+    private lateinit var profileBottomSheetBehavior: BottomSheetBehavior<View>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -29,5 +34,20 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         lifecycle.addObserver(profileViewModel)
+        setupBottomSheets(view)
+    }
+
+    private fun setupBottomSheets(view: View) {
+        profileBottomSheet = view.findViewById(R.id.profileBottomSheet)
+        profileBottomSheetBehavior = BottomSheetBehavior.from(profileBottomSheet)
+
+        binding.btnProfileImage.setOnClickListener {
+            profileBottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        }
+
+        binding.profileBottomSheet.btnSaveSettings.setOnClickListener {
+            profileBottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+            Toast.makeText(context, "Settings were saved", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/filmstoday/utils/ActorsBottomSheetBinder.kt
+++ app/src/main/java/com/example/filmstoday/utils/ActorsBottomSheetBinder.kt
@@ -1,15 +1,13 @@
 package com.example.filmstoday.utils
 
 import com.example.filmstoday.R
-import com.example.filmstoday.databinding.ActorBottomSheetLayoutBinding
-import com.example.filmstoday.fragments.FullMovieFragment
+import com.example.filmstoday.databinding.ActorBottomSheetBinding
 import com.example.filmstoday.models.cast.ActorFullInfoModel
 import com.squareup.picasso.Picasso
-import kotlin.reflect.KProperty
 
 class ActorsBottomSheetBinder {
 
-    fun bindActor(actorBottomSheet: ActorBottomSheetLayoutBinding, actor: ActorFullInfoModel) {
+    fun bindActor(actorBottomSheet: ActorBottomSheetBinding, actor: ActorFullInfoModel) {
         actorBottomSheet.tvActorFullName.text = actor.name
         actorBottomSheet.tvActorBiography.text = actor.biography
         actorBottomSheet.tvBirthday.text = actor.birthday
